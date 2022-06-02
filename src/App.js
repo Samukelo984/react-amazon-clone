4d34dbb2ba2c35bca38395af907e80335425cfcc
Samukelo <104824785+Samukelo984@users.noreply.github.com>
2022-06-02 12:49:19 +0200

    implemented page routing funcionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {Route} from "react-router-dom"; 
+import {Route, Switch} from "react-router-dom"; 
 import Header from "./Components/Layout/Header"; 
 import Home from "./Components/Home/Home"
 import Products from './Components/Products/Products';
+import ProductDetails from './Components/ProductDetails';
  
 const App = () => { 
   return (
@@ -10,13 +11,20 @@ const App = () => {
       <Header /> 
 
     <main> 
+      <Switch> 
      <Route path="/home">
       <Home />
     </Route>
 
-    <Route path="/products">
+    <Route path="/products" exact> 
       <Products />
     </Route>
+
+    <Route path="/products/:id">
+      <ProductDetails />  
+    </Route> 
+    </Switch> 
+
     </main>
     </div>
   );
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,9 +1,17 @@
 import React from "react"; 
+import {Link} from "react-router-dom"; 
 
 const Products = () => {
   return (
      <div>  
     <h1>Product list</h1>
+
+    <ul>
+      <li><Link to="/products/01">Product 1</Link></li>
+      <li><Link to="/products/02">Product 2</Link></li>
+      <li><Link to="/products/03">Product 3</Link></li>
+    </ul>
+ 
      </div> 
   );
 };
