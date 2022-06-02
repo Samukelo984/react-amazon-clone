587f952651bd2a9ab540df3371e3a6d7c4ad735f
Samukelo <104824785+Samukelo984@users.noreply.github.com>
2022-06-02 11:58:03 +0200

    2-commits. 1- Added additional pages for navbar. 2- Added initial css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import {Route} from "react-router-dom"; 
+import Header from "./Components/Layout/Header"; 
+import Home from "./Components/Home/Home"
+import Products from './Components/Products/Products';
+ 
+const App = () => { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <Header /> 
+
+    <main> 
+     <Route path="/home">
+      <Home />
+    </Route>
+
+    <Route path="/products">
+      <Products />
+    </Route>
+    </main>
     </div>
   );
-}
+};    
 
 export default App;
+
